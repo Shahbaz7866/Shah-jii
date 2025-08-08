be88969a0303b99d4ae0d72d303d02e04dc70c91
Shahbaz7866 <125943097+Shahbaz7866@users.noreply.github.com>
2025-08-08 22:23:40 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Knight Bot',
+  author: '‎',
+  botName: "Knight Bot",
+  botOwner: 'Shah Botz', // Your name
+  ownerNumber: '923087060547', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.8",
+};
+
+module.exports = settings;
